2c3a2cc09206190123f6c185f33bc9e83a44050f
mohammad-kk <mohammad.k.k00@gmail.com>
2025-02-27 10:53:59 -0800

    meow4

--- igprofileviewer/web/wsgi.py
+++ igprofileviewer/web/wsgi.py
@@ -1,4 +1,13 @@
-from igprofileviewer.web.app import app
+import os
+import sys
+from pathlib import Path
+
+# Add the project root to Python path
+current_dir = Path(__file__).resolve().parent
+project_root = current_dir.parent.parent
+sys.path.append(str(project_root))
+
+from app import app
 
 if __name__ == "__main__":
     app.run()
